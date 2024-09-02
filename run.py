e7c5b28b9376858429c59e89afc6c2d5d562b673
codewithjason <jasonyen199538@gmail.com>
2024-09-02 15:16:08 +0800

    加入分頁並建立連結

--- run.py
+++ run.py
@@ -8,21 +8,36 @@ def home_page():
     return render_template("home.html")
 
 
-@app.route("/hello")
-def hello():
-    return "<h1>Hello</h1>"
+@app.route("/sql")
+def get_sql():
+    return render_template("sql.html")
 
 
-@app.route("/<name>")
-def by_name(name):
-    return f"<h1>{name}, Welcome. This is my Home Page!</h1>"
+@app.route("/github")
+def get_git():
+    return render_template("Github.html")
 
 
-@app.route("/data/appinfo/str/<name>")
-def querryDataMessageByName(name):
-    print("type(name):", type(name))
-    return f"<h1>String => {name}</h1>"
+@app.route("/HTML")
+def get_html():
+    return render_template("HTML.html")
+
+
+@app.route("/css")
+def get_css():
+    return render_template("css.html")
+
+
+@app.route("/js")
+def get_js():
+    return render_template("js.html")
+
+
+# @app.route("/data/appinfo/str/<name>")
+# def querryDataMessageByName(name):
+#     print("type(name):", type(name))
+#     return f"<h1>String => {name}</h1>"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
